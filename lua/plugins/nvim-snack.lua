d486a34a4fc2f94c8e18394401b5de3fc90b023d
mahmood <mahmood@debian.mahmood>
2025-02-14 01:49:25 +0330

    add new plugins

--- lua/plugins/nvim-snack.lua
+++ lua/plugins/nvim-snack.lua
@@ -1,26 +1,29 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	---@type snacks.Config
-	opts = {
-		notifier = {
-			enabled = true,
-			timeout = 5000,
-		},
-		styles = {
-			notification = {
-				wo = { wrap = true }, -- Wrap notifications
-			},
-		},
-	},
-	keys = {
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-	},
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        --[[
+        notifier = {
+            enabled = true,
+            timeout = 5000,
+        },
+        styles = {
+            notification = {
+               wo = { wrap = true }, -- Wrap notifications
+            },
+        },
+        ]]
+        --
+    },
+    keys = {
+        {
+            "<leader>gg",
+            function()
+                Snacks.lazygit()
+            end,
+            desc = "Lazygit",
+        },
+    },
 }
